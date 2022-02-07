36bb20569da24f7e235754c29c1c5d082c5f32b0
Serhii Dovhyi <dovhyi.serhii@gmail.com>
2022-02-07 04:10:24 +0200

    fix: Header menu links style

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 This project was generated using [Nx](https://nx.dev).
 
-Для запуску застосунку
+# Start database script with Docker:
+
+npm run start:dev:db
+
+# Start application:
 
 npm install
-
 npm run movies
---
+++ apps/restapi/src/app/db/app.dbconfig.ts
@@ -0,0 +1,14 @@
+// import {TypeOrmModuleOptions} from "typeorm";
+
+// export function createTypeOrmProdConfig(): TypeOrmModuleOptions{
+//     return({
+//        type: "postgres",
+//        username: "conduit",
+//        password: "conduit",
+//        database: "conduit"
+//        entities: [join(__dirname, '**', '*.entity.{ts, js}')],
+//        synchronize: true,
+//        logging: true,
+//        logger: "advanced-console",
+//    });
+// }
